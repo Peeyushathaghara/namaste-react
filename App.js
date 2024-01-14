5004a4133c88fc34ca85a674719e627273e853e5
Peeyush <peeyush132@gmail.com>
2024-01-14 20:34:25 +0530

    header

--- App.js
+++ App.js
@@ -1,17 +1,65 @@
-const heading = React.createElement('parent', {
-    "id": "parent",
-    "abcp": "eaxy"
-}, [ 
-    (React.createElement('child1', {"id":"child1"}, 
-    [(React.createElement('h1', {}, 'I m an h1 tag')),
-    (React.createElement('h2', {}, 'I m an h2 tag'))])),
-    (React.createElement('child2', {"id":"child2"}, 
-    [(React.createElement('h1', {}, 'I m an h1 tag')),
-    (React.createElement('h2', {}, 'I m an h2 tag'))]))
-]
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement('parent', {
+//     "id": "parent",
+//     "abcp": "eaxy"
+// }, [
+//     (React.createElement('child1', {"id":"child1"},
+//     [(React.createElement('h1', {}, 'I m an h1 tag')),
+//     (React.createElement('h2', {}, '*&&&&&*****&&&&&&'))])),
+//     (React.createElement('child2', {"id":"child2"},
+//     [(React.createElement('h1', {}, 'I m an h1 tag')),
+//     (React.createElement('h2', {}, 'I m an h2 tag'))]))
+// ]
+// )
+
+//using JSX
+// babel behind the scenes converts JSX code to React.createElement
+
+// Headers
+//     logo 
+//     nav-items 
+// Body 
+//     search 
+//     restaurant container 
+//     - restaurant card 
+// Footer
+//     copyright 
+//     links 
+//     address 
+//     contact 
+
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+            <img className="logo" src = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Applayout = () =>{
+    return (
+        <div className="app">
+            <Header />
+
+        </div>
+    )
+}
 
 // console.log(heading)
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(root)
-root.render(heading)
+root.render(<Applayout />);
--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 # git branch -M branchname
 # git add .
 # git commit -m "type your message here"
+# git remote add origin https://github.com/Peeyushathaghara/namaste-react.git
+# git push origin main
+
+<!-- important json packages in our package.json file
+first command npm init - to create package  -->
+
+<!-- we need a bumdler eg webpack, parcel -->
+
+important npm commands
+
+npm install -D packagename ->  -D flag stands for dependencies. There are 2 types of dependencies -devdependencies used for development purposes whereas as dependencies are used for production purposes.
+installing parcel as devdependencies
+npm install -D parcel
+
+npx parcel sourcefile
+npx parcel build entryfile - for building production file
+
+in package.json mention under the scripts key different commands and then run using...
+npm run scriptkeyname
+
