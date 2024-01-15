b477b90314b3461bba65c8be5a0c2ad8ff5bdb70
Peeyush <peeyush132@gmail.com>
2024-01-15 08:50:22 +0530

    res-card

--- App.js
+++ App.js
@@ -18,46 +18,80 @@ import ReactDOM from "react-dom/client";
 // babel behind the scenes converts JSX code to React.createElement
 
 // Headers
-//     logo 
-//     nav-items 
-// Body 
-//     search 
-//     restaurant container 
-//     - restaurant card 
+//     logo
+//     nav-items
+// Body
+//     search
+//     restaurant container
+//     - restaurant card
 // Footer
-//     copyright 
-//     links 
-//     address 
-//     contact 
-
-
+//     copyright
+//     links
+//     address
+//     contact
 
 const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-container">Search</div>
+      <div className="res-container">
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+        <ResCard />
+      </div>
+    </div>
+  );
+};
+
+const ResCard = () =>{
     return(
-        <div className="header">
-            <div className="logo-container">
-            <img className="logo" src = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
+        <div className="res-card">
+            <img alt="res-logo" className="res-logo" src="https://b.zmtcdn.com/data/dish_photos/6d0/a959f51d106c8c56d915f292c61946d0.jpg" />
+            <h3>Bansal Sweets</h3>
+            <h4>North Indian, Chinese, Kathiawadi</h4>
+            <h5>4.3 stars</h5>
+            <h6>38min ETA</h6>
         </div>
     )
 }
-
-const Applayout = () =>{
-    return (
-        <div className="app">
-            <Header />
-
-        </div>
-    )
-}
+const Applayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 // console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById("root"));
