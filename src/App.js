adb1be8a176a182ecf1d3b513fc2f6dd3c41c081
Peeyush Athaghara <peeyush132@gmail.com>
2024-02-01 09:44:45 +0530

    outlet added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from './components/Header'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Body from "./components/Body";
 import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const Applayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -20,16 +20,23 @@ const AppRouter = createBrowserRouter([
   {
     path:"/",
     element: <Applayout />,
-    errorElement: <Error />
-  },
-  {
-    path:"/about",
-    element: <About />
-  },
-  {
-    path:"/contact",
-    element: <Contact />
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element : <Body/>
+      },
+      {
+        path:"/about",
+        element: <About />
+      },
+      {
+        path:"/contact",
+        element: <Contact />
+      }
+    ]
   }
+  
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
